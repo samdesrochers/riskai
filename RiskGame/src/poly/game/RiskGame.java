4cb63d615202b1ce623adf9f2c2ee0ee9f934ca7
samdesrochers <sam.desrochers@gmail.com>
2013-03-07 19:49:22 -0500

    Synchron. fix for map display

--- RiskGame/src/poly/game/RiskGame.java
+++ RiskGame/src/poly/game/RiskGame.java
@@ -226,6 +226,13 @@ public class RiskGame extends Canvas{
 
 		// Attack other territories
 		executeAttackPhase();
+		
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		// Hand out a card if necessary
 		executePostAttackPhase(currentNbTerritories);
